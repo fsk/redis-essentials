3ed8a17337e6b8bc8e74bd9d43e3ee835da9fe89
fsk <fsk@fskdev.co>
2024-01-04 12:40:40 +0300

    code refactoring

--- 3-third-example/queue.js
+++ 3-third-example/queue.js
@@ -13,18 +13,8 @@ Queue.prototype.push = function (data) {
     return this.redisClient.lPush(this.queueKey, data);
 }
 
-Queue.prototype.pop = function() {
-    return new Promise((resolve, reject) => {
-        this.redisClient.brPop(this.queueKey, this.timeOut, (err, reply) => {
-            if (err) {
-                console.error("Redis brPop Error:", err);
-                reject(err);
-            } else {
-                console.log("Redis brPop Success:", reply);
-                resolve(reply);
-            }
-        });
-    });
+Queue.prototype.pop = function(callback) {
+    this.redisClient.brPop(this.queueKey, this.timeOut, callback);
 };
 
 export default Queue;
