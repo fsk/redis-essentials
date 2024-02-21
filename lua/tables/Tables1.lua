65978411c348cd645e34915680801ffc17ce8b9f
fsk <furkankulaksiz68@gmail.com>
2024-02-21 16:59:49 +0300

    Redis Command

---
+++ lua/tables/Tables1.lua
@@ -0,0 +1,4 @@
+local programmingLanguages = {lua = 1993, javascript = 1995, python = 1991, ruby = 1995}
+
+print("Lua was created in " .. programmingLanguages["lua"])
+print("JavaScript was created in " .. programmingLanguages.javascript)
